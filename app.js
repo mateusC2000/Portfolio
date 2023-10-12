7f8e9a57fc68d83e3e9f626944e438da777348fe
Mateus <mateus.campos@rebase.com>
2023-10-12 00:39:54 -0300

    Add mobile menu and JS behavior.

---
+++ app.js
@@ -0,0 +1,24 @@
+$('nav a, .my-contact a').click(function(e){
+  e.preventDefault()
+  var id = $(this).attr('href');
+    targetOffset = $(id).offset().top;
+  $('html, body').animate({
+    scrollTop: targetOffset
+  }, 500);
+});
+
+let buttonOpen = document.getElementById('button-open')
+let menuMobile = document.getElementById('menu-mobile')
+let overlayMenu = document.getElementById('overlay-menu')
+
+buttonOpen.addEventListener('click', ()=>{
+  menuMobile.classList.add('open-menu')
+})
+
+menuMobile.addEventListener('click', ()=>{
+  menuMobile.classList.remove('open-menu')
+})
+
+overlayMenu.addEventListener('click', ()=>{
+  menuMobile.classList.remove('open-menu')
+})
